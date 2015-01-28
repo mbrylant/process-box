d13664d0ba8c2cf1089585120464c5b66b0e27ff
mbrylant <mbrylant@gmail.com>
2015-01-28 11:10:45 +0000

    ContainerInitializtionException

--- src/main/java/org/jboss/jbpm/processbiox/container/ContainerInitializationException.java
+++ src/main/java/org/jboss/jbpm/processbiox/container/ContainerInitializationException.java
@@ -5,28 +5,18 @@ public class ContainerInitializationException extends Exception {
 	private static final long serialVersionUID = -3444596545598617638L;
 
 	public ContainerInitializationException() {
-		// TODO Auto-generated constructor stub
 	}
 
 	public ContainerInitializationException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
 	}
 
 	public ContainerInitializationException(Throwable cause) {
 		super(cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public ContainerInitializationException(String message, Throwable cause) {
 		super(message, cause);
-		// TODO Auto-generated constructor stub
-	}
-
-	public ContainerInitializationException(String message, Throwable cause,
-			boolean enableSuppression, boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
 	}
 
 }
